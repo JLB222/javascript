673dba05d9283e352ce088785427ba5eaf26d01c
Joshua Brown <JLBrown222@gmail.com>
2022-08-04 11:49:41 -0500

    day 2, 3 more problems

--- javascript.js
+++ javascript.js
@@ -32,3 +32,51 @@ function findShort(s){
   }
   return shortest.length
 }
+
+// 8 - Get the sum of two different array's elements
+
+function arrayPlusArray(arr1, arr2) {
+  return arr1.concat(arr2).reduce((a,b) => a+b)
+}
+
+// 8 - fill an array with every number from 1 to n
+
+function count(n) {
+  let arr = []
+  for (let i = 1; i <= n; i++) {
+    arr.push(i)
+  }
+  return arr
+}
+
+/*
+Our football team finished the championship. The result of each match look like "x:y". Results of all matches are recorded in the collection.
+
+For example: ["3:1", "2:2", "0:1", ...]
+
+Write a function that takes such collection and counts the points of our team in the championship. Rules for counting points for each match:
+
+if x > y: 3 points
+if x < y: 0 point
+if x = y: 1 point
+Notes:
+
+there are 10 matches in the championship
+0 <= x <= 4
+0 <= y <= 4
+*/
+
+function points(games) {
+  let points = 0;   // set starting points
+  for (let i = 0; i < games.length; i++) {  //loop through array
+    if (games[i].split(":")[0] > games[i].split(":")[1]) {  //since array elements are all strings separated by a :, use : as the split
+      points += 3
+      console.log(games[i].split(":")[0])
+    } else if (games[i].split(":")[0] === games[i].split(":")[1]) {  //each array created by split is [x,y].  so [0] and [1] will target them
+      points += 1
+    } else {
+      points += 0
+    }
+  }
+  return points
+}  //alternatively, I could have used map() and used the [0] and [2] positions of the strings.  Ah, the joys of thinking too hard.
