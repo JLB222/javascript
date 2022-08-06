11421371800213e96b661f268d17303d9c691f76
Joshua Brown <JLBrown222@gmail.com>
2022-08-06 13:49:12 -0500

    Day 4

--- javascript.js
+++ javascript.js
@@ -111,3 +111,22 @@ function rentalCarCost(d) {
     return d * cost
   }
 }
+
+//Day 4
+// 8 - Find the average and return the letter of the final grade
+function getGrade (s1, s2, s3) {
+  let avg = (s1 + s2 + s3) / 3
+  let grade = ""
+  if (avg <=100 && avg >=90) {
+    grade = "A"
+  } else if(avg <90 && avg >=80) {
+    grade = "B"
+  } else if(avg <80 && avg >=70) {
+    grade = "C"
+  } else if(avg <70 && avg >=60) {
+    grade = "D"
+  } else {
+    grade = "F"
+  }
+  return grade
+}
