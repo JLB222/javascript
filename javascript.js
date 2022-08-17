8a07a85e1fae931b56b57c7d668d6a7723968852
Joshua Brown <JLBrown222@gmail.com>
2022-08-17 13:35:41 -0500

    Day 14

--- javascript.js
+++ javascript.js
@@ -359,3 +359,21 @@ function howMuchILoveYou(nbPetals) {
     }
     return phrase[nbPetals % 6]
 }
+
+//Day 14
+// 8 - You will be given a list of strings. You must sort it alphabetically (case-sensitive, and based on the ASCII values of the chars) and then return the first value. The returned value must be a string, and have "***" between each of its letters. You should not remove or add elements from/to the array.
+function twoSort(s) {
+  return s.sort()[0].split("").join("***")
+}
+
+// 8 - In this simple exercise, you will create a program that will take two lists of integers, a and b. Each list will consist of 3 positive integers above 0, representing the dimensions of cuboids a and b. You must find the difference of the cuboids' volumes regardless of which is bigger.
+//For example, if the parameters passed are ([2, 2, 3], [5, 4, 1]), the volume of a is 12 and the volume of b is 20. Therefore, the function should return 8.
+
+function findDifference(a, b) {
+  return Math.abs(a[0]*a[1]*a[2] - b[0]*b[1]*b[2])
+}
+
+//Make a function to see if a string is a palindrome.  Arguments will be single words only.  (no sentences)
+function isPalindrome(x) {
+  return x.toLowerCase() === x.toLowerCase().split("").reverse().join("")
+}
