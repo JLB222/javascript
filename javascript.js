578417734e0a5d3d3dee7e476d912dd59967faf4
Joshua Brown <JLBrown222@gmail.com>
2022-09-01 12:32:04 -0500

    day 26

--- javascript.js
+++ javascript.js
@@ -639,3 +639,36 @@ function multiTable(number) {
   }
   return result
 }
+
+//day 26
+//8 - given an array and a number, return the first n elements of said array
+function take(arr, n) {
+  return arr.splice(0, n)
+}
+//8 given the pre-defined functions and variables, make every function return 100 using the correct variables
+var v1=50;v2=100,v3=150,v4=200,v5=2,v6=250
+function equal1(){
+  var a=v1;
+  var b=v1;
+  return a+b;
+}
+function equal2(){
+  var a=v3;
+  var b=v1;
+  return a-b;
+}
+function equal3(){
+  var a=v1;
+  var b=v5;
+  return a*b;
+}
+function equal4(){
+  var a=v4;
+  var b=v5;
+  return a/b;
+}
+function equal5(){
+  var a=v6;
+  var b=v3;
+  return a%b;
+}
