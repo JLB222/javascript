5cab3f98c640046ea13cf4bbe7f583bb8a981f2f
Joshua Brown <JLBrown222@gmail.com>
2022-10-02 12:42:32 -0500

    Day 57

--- javascript.js
+++ javascript.js
@@ -1642,3 +1642,18 @@ function cookie(x){
   }
   return `Who ate the last cookie? It was ${whoDunIt}!`
 }
+
+//Day 57
+function checkExam(array1, array2) {
+  let result = 0
+ for (let i = 0; i < array2.length; i++) {
+   if (array2[i] === array1[i]) {
+     result += 4
+   } else if (array2[i] == "") {
+     result = result
+   } else {
+     result -= 1
+   }
+ }
+  return result < 0 ? 0 : result
+}
