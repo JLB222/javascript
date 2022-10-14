70bbc636c80ec4ce7bed37f540f7b425cda5e395
Joshua Brown <JLBrown222@gmail.com>
2022-10-14 13:01:30 -0500

    Nice

--- javascript.js
+++ javascript.js
@@ -1931,3 +1931,21 @@ var uniqueInOrder=function(iterable){
 var uniqueInOrder=function(iterable){
   return [...iterable].filter((a, i) => a !== iterable[i-1])
 }
+
+//Day 69
+//6 - Equal Sides of an Array:  Given an array of integers, positive and negative, return the index of the array where all numbers to the left of said index and all numbers to the right of it are equal
+function findEvenIndex(arr){
+  let left = 0;
+  let right = arr.reduce((a,b) => a+b,0)
+  
+  for (let i = 0; i < arr.length; i++) {
+    left += arr[i]
+    if (i > 0) {
+      right -= arr[i-1]
+    }
+    if (left === right) {
+      return i
+    }
+  }
+  return -1
+}
