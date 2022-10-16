78e6168287bd5f883aecceb7f85d969febc92016
Joshua Brown <JLBrown222@gmail.com>
2022-10-16 12:13:48 -0500

    Day 71

--- javascript.js
+++ javascript.js
@@ -1977,3 +1977,31 @@ function towerBuilder(nFloors) {
   }
   return result
 }
+
+//Day 71
+class CharacterMaker{ 
+  constructor(name,race,element, skill1Name) {
+    this.name = name,
+    this.race = race,
+    this.element = element
+    this.skill1 = skill1Name
+  }
+  useSkill1() {
+    console.log(`Using ${this.skill1}`)
+  }
+}
+
+//7 - Your task is to split the chocolate bar of given dimension n x m into small squares. Each square is of size 1x1 and unbreakable. Implement a function that will return minimum number of breaks needed.
+//For example if you are given a chocolate bar of size 2 x 1 you can split it to single squares in just one break, but for size 3 x 1 you must do two breaks.
+//If input data is invalid you should return 0 (as in no breaks are needed if we do not have any chocolate to split). Input will always be a non-negative integer.
+function breakChocolate(n,m) {
+  if (n > 0 && m > 0) {
+      return n*m-1;
+  }
+  return 0
+}
+
+//alternate
+var breakChocolate = function(n, m) {
+  return Math.max(0, n*m - 1);
+}
