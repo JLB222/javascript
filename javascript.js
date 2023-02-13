ca5b17f017bd5bfe6c65222e5d88c59d407a0a6c
Joshua Brown <JLBrown222@gmail.com>
2023-02-13 15:03:30 -0600

    Day 190

--- javascript.js
+++ javascript.js
@@ -3909,3 +3909,19 @@ function zeroToNine(n) {
 Math.roundTo = function (number, precision) {
   return +number.toFixed(precision)
 }
+
+//Day 190
+class Person {
+  constructor(firstName = "John", lastName = "Doe", age = 0, gender = "Male") {
+    this.firstName = firstName,
+    this.lastName = lastName,
+    this.age = age,
+    this.gender = gender
+  }
+  sayFullName() {
+    return `${this.firstName} ${this.lastName}`
+  }
+  static greetExtraTerrestrials(raceName) {
+    return `Welcome to Planet Earth ${raceName}`
+  }
+}
