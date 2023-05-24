fe002144c06f99937289814b32d4a6773bea96ec
Josh Brown <JLBrown222@gmail.com>
2023-05-24 11:57:49 -0500

    Day 287

--- javascript.js
+++ javascript.js
@@ -5139,3 +5139,9 @@ function blackAndWhite(arr){
   }
   return "It's a fake array"
 }
+
+//Day 287
+//8 - pressure calc
+solution= (molarMass1, molarMass2, givenMass1, givenMass2, volume, temp) => {
+  return (((givenMass1/molarMass1) + (givenMass2/molarMass2)) * 0.082 * (temp+273.15)) / volume
+}
