33e290a31044ab83a58e7fa41e6f406afe611bd5
Josh Brown <JLBrown222@gmail.com>
2023-06-09 10:40:13 -0500

    Day 303

--- javascript.js
+++ javascript.js
@@ -5341,3 +5341,18 @@ function weatherInfo (temp) {
 function convertToCelsius(temperature) {
   return ((temperature - 32) * (5/9))
 }
+
+//Day 303
+//8 Bug Fixing
+function evalObject(value){
+  var result = 0;
+  switch(value.operation){
+    case'+': result = value.a + value.b; break;
+    case'-': result = value.a - value.b; break;
+    case'/': result = value.a / value.b; break;
+    case'*': result = value.a * value.b; break;
+    case'%': result = value.a % value.b; break;
+    case'^': result = Math.pow(value.a, value.b); break;
+  }
+  return result;
+}
