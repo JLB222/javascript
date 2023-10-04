6df9648083fdea0fb20ca907bf301b43ba7edd39
Josh Brown <JLBrown222@gmail.com>
2023-10-04 13:54:23 -0500

    Day 420

--- javascript.js
+++ javascript.js
@@ -6802,3 +6802,9 @@ String.prototype.eachChar = function(el) {
     return this.split("").map(el).join("")
   }
 }
+
+//Day 420
+//7 - 
+String.prototype.eachChar = function(el) {
+  return typeof el === "string" ? this.split("").map(x => x+el).join("") : this.split("").map(el).join("")
+}
