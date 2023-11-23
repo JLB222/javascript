cae7c66fe265a9a09f12aef823b01101de9bdcf4
Josh Brown <JLBrown222@gmail.com>
2023-11-23 12:31:55 -0600

    Day 469

--- javascript.js
+++ javascript.js
@@ -7446,3 +7446,18 @@ function sortGrades(lst){
   const grades = ["VB", "V0", "V0+", "V1", "V2", "V3", "V4", "V5", "V6", "V7", "V8", "V9", "V10", "V11", "V12", "V13", "V14", "V15", "V16", "V17"];
   return lst.sort((a, b) => grades.indexOf(a) - grades.indexOf(b));
 }
+
+//Day 469
+//review
+class GBFMaker {
+  constructor(name, race, gender, element, ultName) {
+    this.name = name
+    this.race = race
+    this.gender = gender
+    this.element = element
+    this.ultName = ultName
+  }
+  ult() {
+    console.log(`Using ${this.ultName}`)
+  }
+}
