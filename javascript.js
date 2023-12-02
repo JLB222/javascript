d153abeccb96e330a65e2608114c6f92f2e72e26
Josh Brown <JLBrown222@gmail.com>
2023-12-02 14:15:33 -0600

    Day 478

--- javascript.js
+++ javascript.js
@@ -7547,3 +7547,12 @@ function sumOfIntegersInString(s){
   return (s.match(/\d+/g) || []).reduce((a,b) => a + +b, 0)
 }
 
+//Day 478
+//review - string scramble (https://www.codewars.com/kata/5822d89270ca28c85c0000f3/train/javascript)
+function scramble(str, arr) {
+  let result = []
+  for (let i = 0; i < str.length; i++) {
+    result[arr[i]] = str[i]
+  }
+  return result.join("")
+}
