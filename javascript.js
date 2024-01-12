1674c35e29c17f83a995172dd2275303bb959b71
Josh Brown <JLBrown222@gmail.com>
2024-01-12 11:08:04 -0600

    Day 519

--- javascript.js
+++ javascript.js
@@ -7999,3 +7999,15 @@ class MakeAThing {
     return `${this.ultName}!!`
   }
 }
+
+//Day 519
+//7 - jumping number
+function jumpingNumber(n){
+  let arr = n.toString().split('')
+  for(i = 0; i < arr.length-1; i++){
+    if(Math.abs(arr[i] - arr[i+1]) !== 1 ){
+      return 'Not!!'
+    }
+  }
+  return 'Jumping!!'
+}
