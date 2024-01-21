196d7fd1dc65da88a68f26aac8cc7ea833ee86bb
Josh Brown <JLBrown222@gmail.com>
2024-01-21 12:44:21 -0600

    Day 528

--- javascript.js
+++ javascript.js
@@ -8099,3 +8099,20 @@ function customSort(array) {
   let desiredSort = '23456789TJQKA'
   return array.sort((a,b) => desiredSort.indexOf(a) - desiredSort.indexOf(b))
 }
+
+//Day 528
+function fizzBuzz(number) {
+  for (let i = 1; i <= number; i++) {
+    if (i % 15 === 0) {
+      console.log("FizzBuzz")
+    } else
+    if (i % 5 === 0) {
+      console.log("Buzz")
+    } else
+    if (i % 3 === 0) {
+      console.log("Fizz")
+    } else {
+      console.log(i)
+    }
+  }
+}
