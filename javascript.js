6362945b325a958c191298fdbf16189bbcd349e5
Josh Brown <JLBrown222@gmail.com>
2024-02-20 11:33:18 -0600

    Day 558

--- javascript.js
+++ javascript.js
@@ -8664,3 +8664,17 @@ function scramble(str, arr) {
   }
   return result.join("")
 }
+
+//Day 558
+//review class constructor
+class gbfCharacterMaker {
+  constructor(name, race, element, ultName) {
+    this.name = name
+    this.race = race
+    this.element = element
+    this.ultName = ultName
+  }
+  ult() {
+    return `${this.ultName}`
+  }
+}
