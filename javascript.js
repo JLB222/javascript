d7def7ba9186b83fd53f4651b8483a3c31b9888c
Josh Brown <JLBrown222@gmail.com>
2024-03-20 13:35:18 -0500

    Day 586

--- javascript.js
+++ javascript.js
@@ -8909,3 +8909,10 @@ function sevenAte9(str) {
   }
   return numbers.join("")
 }
+
+//Day 586
+//array.from(arrayLike, mapFn, thisArg))
+console.log(Array.from('foo'));
+// Expected output: Array ["f", "o", "o"]
+console.log(Array.from([1, 2, 3], (x) => x + x));
+// Expected output: Array [2, 4, 6]
