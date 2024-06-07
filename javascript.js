5587f5304256abb3fdd3d25ea8168e981357259d
Josh Brown <JLBrown222@gmail.com>
2024-06-07 10:00:57 -0500

    Day 662

--- javascript.js
+++ javascript.js
@@ -9564,3 +9564,22 @@ const str661 = 'Breaded Mushrooms'
 
 console.log(str661.padEnd(25, '.')); // Expected output: "Breaded Mushrooms........"
 console.log(str661.padEnd(25)); // Expected output: "Breaded Mushrooms        "
+
+//Day 662
+//7 - Basic Math (Add or Subtract) - given a string of numbers and operators, return the sum as a string.  ex: "1plus2plus3plus4"  --> "10"
+function calculate(str) {
+  let numbers = str.split(/plus|minus/);
+  let operations = str.match(/[a-z]+/g);
+  let sum = parseInt(numbers[0]);
+  for(let i = 0, len = operations.length; i < len; i++){
+    switch(operations[i]){
+      case "plus":
+        sum += parseInt(numbers[i+1]);
+        break;
+      case "minus":
+        sum -= parseInt(numbers[i+1]);
+        break;
+    }
+  }
+  return sum+"";
+}
