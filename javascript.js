925655e70891ca0fcc553f71b2709ff5d8a58c64
Josh Brown <JLBrown222@gmail.com>
2024-06-22 12:43:13 -0500

    Day 677

--- javascript.js
+++ javascript.js
@@ -9715,3 +9715,8 @@ function mygcd(x,y){
 function maxProduct(arr, num){
   return arr.sort((a,b) => b-a,0).slice(0, num).reduce((prev,curr) => prev*curr, 1)
 }
+
+//Day 677
+//String.split() - takes a pattern and divides this string into an ordered list of substrings by searching for the pattern, puts these substrings into an array, and returns the array
+'The quick brown fox jumps over the lazy dog.'.split(" ") //should return ['The', 'quick', 'brown', 'fox', 'jumps', 'over', 'the', 'lazy', 'dog.']
+
