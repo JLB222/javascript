b20d6adfa8924b7a115a895aec1f6843fee90c15
Josh Brown <JLBrown222@gmail.com>
2024-10-03 13:15:34 -0500

    Day 779

--- javascript.js
+++ javascript.js
@@ -10663,3 +10663,16 @@ function customSort777(arr) {
 //Day 778
 //Number.parseFloat() - parses an argument and returns a floating point number. If a number cannot be parsed from the argument, it returns NaN.
 console.log(Number.parseFloat(2.0 * Math.PI))
+
+//Day 779
+//review
+function hasScored779(str) {
+  let cushionsHit = 0
+  let ballsHit = new Set()
+  for (let char of str) {
+    if ("news".includes(char)) {cushionsHit++}
+    else {ballsHit.add(char)}
+    if (ballsHit.size == 2 && ballsHit.has("R")) {return cushionsHit >= 3}
+  }
+  return false
+}
