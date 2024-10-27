af3be7a0743e44bf930a4cc30f844ad559d5fae5
Josh Brown <JLBrown222@gmail.com>
2024-10-27 12:46:22 -0500

    Day 803

--- javascript.js
+++ javascript.js
@@ -11048,3 +11048,36 @@ class Clock {
     this.timer = setInterval(() => this.render(), 1000);
   }
 }
+
+//Day 803
+//review - classes
+class Animal {
+  constructor(name) {
+    this.speed = 0;
+    this.name = name;
+  }
+  run(speed) {
+    this.speed = speed;
+    alert(`${this.name} runs with speed ${this.speed}.`);
+  }
+  stop() {
+    this.speed = 0;
+    alert(`${this.name} stands still.`);
+  }
+}
+
+class Rabbit extends Animal {
+  constructor(name, earLength) {
+    super(name);
+    this.earLength = earLength;
+  }
+  
+  hide() {
+    alert(`${this.name} hides!`);
+  }
+
+  stop() {
+    super.stop(); // call parent stop
+    this.hide(); // and then hide
+  }
+}
