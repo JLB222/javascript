6e3a9ef79a808b9ba2c397336cc548958ef5f309
Josh Brown <JLBrown222@gmail.com>
2024-11-05 17:56:01 -0600

    Day 812

--- javascript.js
+++ javascript.js
@@ -11259,3 +11259,14 @@ function returnPairs(arr, num) {
   }
   return result
 }
+
+//Day 812
+//7 - A letter's best friend
+function bestFriend(txt, a, b) {
+  for (let i = 0; i < txt.length; i++) {
+    if (txt[i] == a && txt[i+1] !== b) {
+      return false
+    }
+  }
+  return true
+}
