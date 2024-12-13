bad9e6b3344429687db1809cfe07aa1f4b84cbbb
Josh Brown <JLBrown222@gmail.com>
2024-12-13 16:13:41 -0600

    Day 849

--- javascript.js
+++ javascript.js
@@ -11738,3 +11738,28 @@ function someButNotAll(seq, pred){
   const arr = Array.from(seq);
   return arr.some(pred) && !arr.every(pred);
 }
+
+//Day 849
+//String.toWellFormed() -  returns a string where all lone surrogates of this string are replaced with the Unicode replacement character U+FFFD.
+const strings = [
+  // Lone leading surrogate
+  "ab\uD800",
+  "ab\uD800c",
+  // Lone trailing surrogate
+  "\uDFFFab",
+  "c\uDFFFab",
+  // Well-formed
+  "abc",
+  "ab\uD83D\uDE04c",
+];
+
+for (const str of strings) {
+  console.log(str.toWellFormed());
+}
+// Logs:
+// "ab�"
+// "ab�c"
+// "�ab"
+// "c�ab"
+// "abc"
+// "ab😄c"
