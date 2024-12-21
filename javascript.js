18863c034377146298ef63bf83732310fee040e1
Josh Brown <JLBrown222@gmail.com>
2024-12-21 14:50:03 -0600

    Day 857

--- javascript.js
+++ javascript.js
@@ -11838,3 +11838,16 @@ function countElements856(arr) {
   }
   return counter
 }
+
+//Day 857
+//review
+function hasScored(str) {
+  let cushionsHit = 0
+  let ballsHit = new Set()
+  for (let char of str) {
+    if ("news".includes(char)) {cushionsHit++}
+    else {ballsHit.add(char)}
+    if (ballsHit.size == 2 && ballsHit.has("R")) {return cushionsHit >= 3}
+  }
+  return false
+}
