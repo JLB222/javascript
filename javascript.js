c7dbcde6411653714c7b7cb8a94374fcefe8419e
Josh Brown <JLBrown222@gmail.com>
2025-01-16 09:10:44 -0600

    Day 883

--- javascript.js
+++ javascript.js
@@ -12087,3 +12087,21 @@ const sumWithInitial882 = array1.reduce(
 );
 
 console.log(sumWithInitial882);  // Expected output: 10
+
+//Day 883
+//review fizzbuzz
+function fizzBuzz883(num) {
+  for (let i = 1; i <= num; i++) {
+    if (i % 15 === 0) {
+      console.log('Fizzbuzz')
+    } else 
+    if (i % 5 === 0) {
+      console.log('Buzz')
+    } else 
+    if (i % 3 === 0) {
+      console.log('Fizz')
+    } else {
+      console.log(i)
+    }
+  }
+}
