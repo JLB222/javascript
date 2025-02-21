37dfa214e593f72db5e0b51ccd172477daa6c826
Josh Brown <JLBrown222@gmail.com>
2025-02-21 17:41:01 -0600

    Day 879

--- javascript.js
+++ javascript.js
@@ -12490,3 +12490,18 @@ fetch('characters.json')
         updateSearchResults(); 
     })
     .catch(error => console.error("Error loading data:", error))
+
+//Day 879
+//2704: To Be Or Not To Be
+function expect(val) {
+  return {
+      toBe: function(val2) {
+          if (val === val2) {return true} 
+          else throw Error ("Not Equal")
+      },
+      notToBe: function(val2) {
+          if (val !== val2) {return true} 
+          else throw Error ("Equal")
+      }
+  }
+};
