3dd30d982a7849ae17cf8aec6d8600b40014091e
Josh Brown <JLBrown222@gmail.com>
2025-03-24 16:19:44 -0500

    Day 910

--- javascript.js
+++ javascript.js
@@ -12818,3 +12818,14 @@ var createCounter = function(num) {
      return num + timesInvoked++
   };
 };
+
+//Day 910
+
+const array910 = [1, 2, 3, 4];
+
+// Fill with 0 from position 2 until position 4
+console.log(array910.fill(0, 2, 4));  // Expected output: Array [1, 2, 0, 0]
+// Fill with 5 from position 1
+console.log(array910.fill(5, 1));  // Expected output: Array [1, 5, 5, 5]
+
+console.log(array910.fill(6));  // Expected output: Array [6, 6, 6, 6]
