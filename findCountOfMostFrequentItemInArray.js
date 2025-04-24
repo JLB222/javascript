43485fc2277191a56259742e80c35160e7f9f1d5
Josh Brown <JLBrown222@gmail.com>
2025-04-24 10:23:13 -0500

    mostFrequentItemCount

---
+++ findCountOfMostFrequentItemInArray.js
@@ -0,0 +1,10 @@
+//Day
+//Find Count of Most Frequent Item in an Array
+function mostFrequentItemCount(arr) {
+    if (arr.length === 0) return 0;
+    let counter = {}
+    for (let i = 0; i < arr.length; i++) {
+      counter[arr[i]] = (counter[arr[i]] || 0) + 1
+    }
+    return Math.max(...Object.values(counter))
+}
--- javascript.js
+++ javascript.js
@@ -13183,3 +13183,13 @@ function contact(hallway) {
 function greet(name) {
   return name ? `hello ${name}!` : null
 }
+
+//Day 940
+function mostFrequentItemCount(arr) {
+  if (arr.length === 0) return 0;
+  let counter = {}
+  for (let i = 0; i < arr.length; i++) {
+    counter[arr[i]] = (counter[arr[i]] || 0) + 1
+  }
+  return Math.max(...Object.values(counter))
+}
