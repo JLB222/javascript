05c3cee04ffc16fafbf1213fb5d981f70e5d2a44
Josh Brown <JLBrown222@gmail.com>
2025-04-27 11:36:05 -0500

    Simple Calculator

--- javascript.js
+++ javascript.js
@@ -13216,3 +13216,23 @@ function distinct(arr) {
 function betterThanAverage(classPoints, yourPoints) {
   return (classPoints.reduce((a,b) => a+b,0) / classPoints.length) < yourPoints
 }
+
+//Day 943
+//8 - Simple Calculator - https://www.codewars.com/kata/5810085c533d69f4980001cf/train/javascript
+function calculator(a,b,sign){
+  if (typeof a === "number" && typeof b === "number") {
+    switch (sign) {
+        case "+": return a + b;
+        break;
+        case "-": return a - b;
+        break;
+        case "*": return a * b;
+        break;
+        case "/": return a / b;
+        break;
+        default: return "unknown value";
+    }
+  } else {
+    return "unknown value"
+  }
+}
