62ce0475ffdb158149c0994b6d4953f3a364541f
Josh Brown <JLBrown222@gmail.com>
2025-05-29 18:23:20 -0500

    switch review

--- javascript.js
+++ javascript.js
@@ -13532,3 +13532,17 @@ function calculate(str) {
 
   return eval(str.replace(/plus/gi, '+').replace(/minus/gi, '-')).toString();
 }
+
+//Day 974
+function howManydays(month){
+  var days;
+  switch (month){
+    case 4:
+    case 6:
+    case 9:
+    case 11: days = 30; break;
+    case 2: days = 28; break;
+    default: days = 31; break;
+  }
+  return days;
+}
