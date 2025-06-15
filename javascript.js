164037ebc1504d1e38b0f6ae821c8e9de61f6fdf
Josh Brown <JLBrown222@gmail.com>
2025-06-15 12:28:01 -0500

    coding meetup 5

--- javascript.js
+++ javascript.js
@@ -13704,3 +13704,13 @@ class Ball991 {
     this.ballType = ballType
   }
 }
+
+//Day 992
+//Coding Meetup 5 - https://www.codewars.com/kata/5828713ed04efde70e000346/train/javascript
+function countLanguages(list) {
+  let counter = {}
+  for (let i = 0; i < list.length; i++) {
+    counter[list[i].language] = (counter[list[i].language] || 0) +1
+  }
+  return counter
+}
