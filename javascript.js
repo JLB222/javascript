05f87fd12a1864f1fcc7a8cf22ec4226ad656a60
Josh Brown <JLBrown222@gmail.com>
2025-06-17 18:02:47 -0500

    Coding meetup 6

--- javascript.js
+++ javascript.js
@@ -13723,3 +13723,9 @@ function complete(str){
     return str + [...str.slice(0,i)].reverse().join('')
   }
 }
+
+//Day 994
+//7 - Coding Meetup 6 - https://www.codewars.com/kata/58287977ef8d4451f90001a0/train/javascript
+function isSameLanguage(list) {
+  return list.every((el,i) => el.language === list[0].language)
+}
