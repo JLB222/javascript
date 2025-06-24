d78887fbfb4365d15d8e0ad26854f27b93328125
Josh Brown <JLBrown222@gmail.com>
2025-06-24 00:18:21 -0500

    shortest word

--- javascript.js
+++ javascript.js
@@ -13803,3 +13803,16 @@ const digitRacers = str => {
                             
   return result.join`\n` + (!stats[0] ? '\nAll digits present' : '');
 }
+
+//Day 1001
+//7 - Shortest Word - https://www.codewars.com/kata/57cebe1dc6fdc20c57000ac9/train/javascript
+function findShort(s){
+  let words = s.split(" ")
+  let shortest = words[0]
+  for (let i = 0; i < words.length; i++) {
+    if (words[i].length < shortest.length) {
+      shortest = words[i]
+    } 
+  }
+  return shortest.length
+}
