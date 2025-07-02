a68704d27ebfeac78a28bb59088c8b3eb39ffde9
Josh Brown <JLBrown222@gmail.com>
2025-07-02 16:48:16 -0500

    Ones and Zeroes

--- javascript.js
+++ javascript.js
@@ -13876,3 +13876,9 @@ class StateMachine1008 {
     })
   }
 }
+
+//Day 1009
+//7 - Ones and Zeroes - https://www.codewars.com/kata/578553c3a1b8d5c40300037c/train/javascript
+const binaryArrayToNumber1009 = arr => {
+  return parseInt(arr.join(""), 2)
+}
---
+++ onesAndZeroes.js
@@ -0,0 +1,5 @@
+//Day 1009
+//7 - Ones and Zeroes - https://www.codewars.com/kata/578553c3a1b8d5c40300037c/train/javascript
+const binaryArrayToNumber = arr => {
+  return parseInt(arr.join(""), 2)
+}
