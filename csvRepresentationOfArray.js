4cd3b28438ae3ab4c1ab182c3a175e99cfc6969c
Josh Brown <JLBrown222@gmail.com>
2025-07-08 17:23:07 -0500

    array to csv

---
+++ csvRepresentationOfArray.js
@@ -0,0 +1,5 @@
+//Day 1015
+//8 - CSV representation of array - https://www.codewars.com/kata/5a34af40e1ce0eb1f5000036/javascript
+function toCsvText(array) {
+   return array.join('\n');
+}
--- javascript.js
+++ javascript.js
@@ -13920,3 +13920,9 @@ function expressionMatter(a, b, c) {
     (a + b) * c,
   );
 }
+
+//Day 1015
+//8 - CSV representation of array - https://www.codewars.com/kata/5a34af40e1ce0eb1f5000036/javascript
+function toCsvText(array) {
+   return array.join('\n');
+}
