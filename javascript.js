0d33cd1ed2c58263819dfa4ed915e8b126c08960
Josh Brown <JLBrown222@gmail.com>
2025-07-16 11:50:36 -0500

    geometryBasics

--- javascript.js
+++ javascript.js
@@ -14024,3 +14024,8 @@ function hasScored1022(str) {
   return false;
 }
 
+//Day 1023
+//8 - Geometry Basics: Distance between 2 points - https://www.codewars.com/kata/58dced7b702b805b200000be/train/javascript
+function distanceBetweenPoints(a, b) { 
+  return Math.sqrt((b.x - a.x) ** 2 + (b.y - a.y) ** 2)
+}
