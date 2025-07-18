1d7ba2b121f6aff955174f1d1bd03c002144f07f
Josh Brown <JLBrown222@gmail.com>
2025-07-18 13:52:06 -0500

    find multiples

--- javascript.js
+++ javascript.js
@@ -14044,3 +14044,13 @@ class StateMachine1024 {
     }
   }
 }
+
+//Day 1025 
+//8 - find multiples of a number - https://www.codewars.com/kata/58ca658cc0d6401f2700045f/train/javascript
+function findMultiples(integer, limit) {
+  let result = []
+  for (let i = 1; i <= limit/integer; i++) {
+    result.push(integer * i)
+  }
+  return result
+}
