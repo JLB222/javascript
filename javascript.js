310a0e99728e3c65a8b0c1d7123546944a78f141
Josh Brown <JLBrown222@gmail.com>
2025-07-24 18:46:22 -0500

    switch it up

--- javascript.js
+++ javascript.js
@@ -14108,3 +14108,9 @@ function mergeArrays(arr1, arr2) {
   combo = [...new Set(combo)]
   return combo.sort((a,b) => a-b)
 }
+
+//Day 1031
+//8 - Switch it Up! - https://www.codewars.com/kata/5808dcb8f0ed42ae34000031/train/javascript
+function switchItUp(number){
+  return ["Zero", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine"][number]
+}
---
+++ switchItUp.js
@@ -0,0 +1,5 @@
+//Day 1031
+//8 - Switch it Up! - https://www.codewars.com/kata/5808dcb8f0ed42ae34000031/train/javascript
+function switchItUp(number){
+  return ["Zero", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine"][number]
+}
