942ba70d46e630da9a631241f2972df61974bcd9
Josh Brown <JLBrown222@gmail.com>
2025-08-29 10:19:26 -0500

    smalled unused id

--- javascript.js
+++ javascript.js
@@ -14480,3 +14480,26 @@ class StateMachine1067 {
     }
   }
 }
+
+//Day 1068
+//8 - Smallest unused ID
+function nextIdA(ids){
+  ids.sort((a,b) => a-b,0)
+  for (let i = 0; i < ids.length; i++) {
+   if (!ids.includes(i)) {
+     return i
+   }
+  }
+  return Math.max(...ids)+1
+}
+
+//OR
+
+function nextIdB(ids){
+  ids.sort((a,b) => a-b,0)
+  for (let i = 0; i <= ids.length; i++) {
+   if (!ids.includes(i)) {
+     return i
+   }
+  }
+}
