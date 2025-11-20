2c9f9f9b01248e1332f69ffbdccc11356a363733
Josh Brown <JLBrown222@gmail.com>
2025-11-20 15:55:18 -0600

    stones on the table

--- javascript.js
+++ javascript.js
@@ -15765,3 +15765,13 @@ function sarcasm1151(sentence) {
 
   return result.join(" ")
 }
+
+//Day 1152
+//7 - Stones on the Table - https://www.codewars.com/kata/5f70e4cce10f9e0001c8995a/train/javascript
+function solve(stones) {
+  let count = 0
+  for (let i = 1; i <= stones.length; i++) {
+    if (stones[i] === stones[i-1]) { count++ }
+  }
+  return count
+}
