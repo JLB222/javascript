5ef1e532d317927ff2e94016a2c4901a532baf32
Josh Brown <JLBrown222@gmail.com>
2025-11-26 19:43:08 -0600

    pick it

--- javascript.js
+++ javascript.js
@@ -15875,3 +15875,12 @@ function handleFullName(event) {
     </div>
   );
 }
+
+//Day 1158
+function pickIt1158(arr){
+  let odd=[],even=[];
+  for (let i = 0; i < arr.length; i++) {
+    arr[i] % 2 === 0 ? even.push(arr[i]) : odd.push(arr[i])
+  } 
+  return [odd,even];
+}
