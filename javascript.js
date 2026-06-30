4ffe1a5787d9d17d67bc0c2588971f6bce6f741d
Josh Brown <JLBrown222@gmail.com>
2026-06-30 08:50:18 -0500

    long name

--- javascript.js
+++ javascript.js
@@ -18430,3 +18430,12 @@ function top3_1203(products, amounts, prices) {
       .slice(0,3)
       .map(el => el.name)
 }
+
+//Day 1204
+//8 - long name - https://www.codewars.com/kata/574b1916a3ebd6e4fa0012e7/train/javascript
+function isOpposite(s1,s2){
+  return s1.split('')
+    .map(c => c === c.toUpperCase() ? c.toLowerCase() : c.toUpperCase())
+    .join('') === s2 && s1 !== '';
+  
+}
