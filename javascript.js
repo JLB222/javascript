73a77e21a2e93501a49c434f98339d2062198dbf
Josh Brown <JLBrown222@gmail.com>
2026-07-07 13:51:32 -0500

    quadrant determination

--- javascript.js
+++ javascript.js
@@ -18536,3 +18536,20 @@ class Dog extends Animal {
     this.breed = breed
   }
 }
+
+//Day 1214
+//r
+function quadrant_1214(x, y) {
+  if (x > 0 && y > 0) {
+    return 1
+  }
+  if (x < 0 && y < 0) {
+    return 3
+  }
+  if (x > 0 && y < 0) {
+    return 4
+  }
+  if (x < 0 && y > 0) {
+    return 2
+  }
+}
