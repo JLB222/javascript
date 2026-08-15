5a1357ae8582fb3f6cf1d9e4463afc852d245e2d
Josh Brown <JLBrown222@gmail.com>
2026-08-15 09:10:24 -0500

    how many days

--- javascript.js
+++ javascript.js
@@ -18934,3 +18934,17 @@ function solve_1249(string) {
   }
   return result;
 };
+
+//Day 1250
+function howManydays_1250(month){
+  let days;
+  switch (month){
+    case 4:
+    case 6:
+    case 9:
+    case 11: days = 30; break;
+    case 2: days = 28; break;
+    default: days = 31; break;
+  }
+  return days;
+}
