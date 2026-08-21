88e43da9125d157075296e8e66bd720757955947
Josh Brown <JLBrown222@gmail.com>
2026-08-21 12:41:25 -0500

    xcom

--- javascript.js
+++ javascript.js
@@ -19001,3 +19001,17 @@ class gbf_1255 {
     console.log(`${this.ultName}!`)
   }
 }
+
+//Day 1256
+//8 - XCOM - https://www.codewars.com/kata/67b7a527c9f842fd3b02adb8/train/javascript
+// speed of aircraft is given in *knots*
+// travelTime is in *minutes*
+// travel distance should be returned in *kilometers*
+function travelDistance(avgSpeed, travelTime) {
+  const KM_PER_MILE = 1.609344;
+  const travelHours = travelTime / 60;
+  const travelMiles = (avgSpeed * 1.15078) * travelHours;  //knots are now converted to MPH
+  const travelKms   = travelMiles * KM_PER_MILE;  //miles now multiplied by KM per mile instead of divided
+  
+  return travelKms;
+}
