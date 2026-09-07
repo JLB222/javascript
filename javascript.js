9d0bafac60ce7d8cc3b8db3261e4f643dc591326
Josh Brown <JLBrown222@gmail.com>
2026-09-07 11:03:38 -0500

    holiday fizz

--- javascript.js
+++ javascript.js
@@ -19133,3 +19133,21 @@ function howManyDays_1268(month) {
   }
   return days
 }
+
+//Day 1269 
+//Labor Day
+function fizzBuzz_1269(num) {
+  for (let i = 1; i <= num; i++) {
+    if (i % 15 === 0) {
+      console.log("Fizzbuzz")
+    } else
+    if (i % 5 === 0) {
+      console.log("Buzz")
+    } else 
+    if (i % 3 === 0) {
+      console.log("Fizz")
+    } else {
+      console.log(i)
+    }
+  }
+}
