72ea11406e3b35da07c39d6021272c04600095f6
Josh Brown <JLBrown222@gmail.com>
2026-09-08 11:05:36 -0500

    divisor count

--- javascript.js
+++ javascript.js
@@ -19151,3 +19151,15 @@ function fizzBuzz_1269(num) {
     }
   }
 }
+
+//Day 1270
+//r
+function getDivisorsCnt_1270(n){
+  let counter = 0
+  for (let i = 1; i <= n; i++) {
+    if (n % i === 0) {
+      counter++
+    }
+  }
+  return counter
+}
